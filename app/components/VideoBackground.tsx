d84d9587e4f605e4f3caeb37faa737bf681cca07
4levy <100963276+4levy@users.noreply.github.com>
2025-06-29 14:23:40 +0700

    Update VideoBackground.tsx

--- app/components/VideoBackground.tsx
+++ app/components/VideoBackground.tsx
@@ -10,7 +10,7 @@ const VideoBackground: React.FC = () => (
     className="fixed top-0 left-0 w-full h-full object-cover  z-0"
     style={{ pointerEvents: "none" }}
   >
-    <source src="/background/Background.mp4" type="video/mp4" />
+    <source src="/background/background.mp4" type="video/mp4" />
     Your browser does not support the video tag.
   </video>
 );
